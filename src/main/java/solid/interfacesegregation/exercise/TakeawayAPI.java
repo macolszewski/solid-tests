55c695f9229b5b91bf337983ea572d2ecfee5bf2
macolszewski <olschevski@gmail.com>
2018-09-16 11:28:50 +0200

    interface integration exercise

--- src/main/java/solid/interfacesegregation/exercise/TakeawayAPI.java
+++ src/main/java/solid/interfacesegregation/exercise/TakeawayAPI.java
@@ -1,15 +1,16 @@
 package solid.interfacesegregation.exercise;
 
-public interface TakeawayAPI extends OnlineAPI, RestaurantAPI{
+public interface TakeawayAPI extends OnlineAPI, RestaurantAPI {
     @Override
     void acceptOnlineOrder();
-    public void takeawayPizza();
-    public void takeTelephoneOrder();
+
+    void takeawayPizza();
+
+    void takeTelephoneOrder();
 
     @Override
     void payOnline();
 
-    @Override
     void payInPerson();
 
     @Override
